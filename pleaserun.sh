1e0814fa5ac3a640006c227dd2df42e63322ded2
Francis Bonneau <francis.bonneau@gmail.com>
2014-11-25 16:25:14 -0500

    added init script

---
+++ pleaserun.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# LSCV (Linux System Call Visualization) project
+# Francis Bonneau, autumn 2014
+# Created for the course GTI792 at l'ÉTS (http://etsmtl.ca/)
+#
+# Script to create a init file using pleaserun 
+# https://github.com/jordansissel/pleaserun
+#
+
+pleaserun --platform sysv --version lsb-3.1 --user root --name lscv-server --chdir /etc/lscv-server sysdig -c lscv-chisel.lua
